b1d0ed5a4b5fc0739c0216f12fdc6902831f1903
Omar Gastelum <gastelumdev@gmail.com>
2023-05-10 11:52:18 -0700

    Add Client

---
+++ client/src/App.tsx
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function App() {
+    const [message, setMessage] = useState("");
+    const [events, setEvents] = useState();
+
+    // Fetching message from backend on mount
+    useEffect(() => {
+        fetch("http://localhost:4000")
+            .then((res) => res.json())
+            .then((data) => setMessage(data.message));
+    }, []);
+
+    const createEvent = async () => {
+        const event = { name: "New Event", description: "New Event Desc" };
+        const createEvent = await axios.post(
+            "http://localhost:4000/create_event",
+            event
+        );
+        const data = createEvent;
+        console.log(event);
+    };
+
+    const getEvents = async () => {
+        const data = (await axios.get("http://localhost:4000/events")) as any;
+        setEvents(data);
+    };
+
+    return (
+        <div className="App">
+            <h1>{message}</h1>
+            {/* {getEvents.map()} */}
+        </div>
+    );
+}
+
+export default App;
