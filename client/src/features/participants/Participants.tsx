d790f27da5879510dac5ab2c204df3ef1010652c
Omar Gastelum <gastelumdev@gmail.com>
2023-05-25 15:01:29 -0700

    Changed Typo

--- client/src/features/participants/Participants.tsx
+++ client/src/features/participants/Participants.tsx
@@ -201,7 +201,7 @@ const Dashboard = () => {
                 <DrawerContent>
                     <DrawerCloseButton />
                     <DrawerHeader borderBottomWidth="1px">
-                        Envite Participant
+                        Invite Participant
                     </DrawerHeader>
 
                     <DrawerBody>
