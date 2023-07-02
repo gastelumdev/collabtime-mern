fc8607ebf6192bb47145ede48b947b1dfaccc1a9
Omar Gastelum <gastelumdev@gmail.com>
2023-07-02 16:29:45 -0700

    Extend form and create export functionality

--- client/src/features/participants/Participants.tsx
+++ client/src/features/participants/Participants.tsx
@@ -45,6 +45,35 @@ const Dashboard = () => {
     const [data, setData] = useState<TParticipant>({
         name: "",
         email: "",
+        street: "",
+        city: "",
+        state: "",
+        zipcode: "",
+        band_director_name: "",
+        band_director_phone: "",
+        band_director_email: "",
+        booster_parent_name: "",
+        booster_parent_phone: "",
+        booster_parent_email: "",
+        parade_march_title: "",
+        parade_march_composer: "",
+        additional_band_staff_names: "",
+        drum_major: "",
+        drum_major_name: "",
+        color_guard_advisor: "",
+        color_guard_captains: "",
+        drill_team: "",
+        drill_team_advisor: "",
+        drill_team_captains: "",
+        school_enrollment: "",
+        number_of_students_in_band: "",
+        number_of_students_in_color_guard: "",
+        number_of_students_in_drill_team: "",
+        number_of_buses: "",
+        number_of_box_trucks: "",
+        number_of_trailers: "",
+        number_of_tractor_trailer_rigs: "",
+        special_instructions: "",
         event: localStorage.getItem("eventId"),
     });
     const [isCreate, setIsCreate] = useState(true);
@@ -125,6 +154,75 @@ const Dashboard = () => {
 
     const handleError = () => {};
 
+    function convertArrayOfObjectsToCSV(array: [TParticipant]) {
+        let result: string;
+
+        const columnDelimiter = ",";
+        const lineDelimiter = "\n";
+        const keys = Object.keys(data);
+
+        result = "";
+        result += keys.join(columnDelimiter);
+        result += lineDelimiter;
+
+        array.forEach((item: TParticipant) => {
+            let ctr = 0;
+            keys.forEach((key: string) => {
+                console.log(key);
+                if (ctr > 0) result += columnDelimiter;
+
+                result += item[key as keyof TParticipant];
+                // eslint-disable-next-line no-plusplus
+                ctr++;
+            });
+            result += lineDelimiter;
+        });
+
+        return result;
+    }
+
+    // Blatant "inspiration" from https://codepen.io/Jacqueline34/pen/pyVoWr
+    function downloadCSV(array: [TParticipant]) {
+        const link = document.createElement("a");
+        let csv = convertArrayOfObjectsToCSV(array);
+        if (csv == null) return;
+
+        const filename = "export.csv";
+
+        if (!csv.match(/^data:text\/csv/i)) {
+            csv = `data:text/csv;charset=utf-8,${csv}`;
+        }
+
+        link.setAttribute("href", encodeURI(csv));
+        link.setAttribute("download", filename);
+        link.click();
+    }
+
+    const Export = ({ onExport }: { onExport: () => void }) => (
+        <Button onClick={() => onExport()}>Export CSV</Button>
+    );
+
+    const ExportCSV = () => {
+        const actionsMemo = React.useMemo(
+            () => (
+                <Export
+                    onExport={() => downloadCSV(participants as [TParticipant])}
+                />
+            ),
+            []
+        );
+
+        return (
+            <DataTable
+                title="Movie List"
+                columns={columns}
+                data={participants}
+                actions={actionsMemo}
+                pagination
+            />
+        );
+    };
+
     type DataRow = {
         _id: string;
         name: string;
@@ -141,6 +239,172 @@ const Dashboard = () => {
             name: "Email",
             selector: (row: { email: any }) => row.email,
             sortable: true,
+        },
+        {
+            name: "Street address",
+            selector: (row: { street_address: any }) => row.street_address,
+            omit: true,
+        },
+        {
+            name: "City",
+            selector: (row: { city: any }) => row.city,
+            omit: true,
+        },
+        {
+            name: "State",
+            selector: (row: { state: any }) => row.state,
+            omit: true,
+        },
+        {
+            name: "Zipcode",
+            selector: (row: { zipcode: any }) => row.zipcode,
+            omit: true,
+        },
+        {
+            name: "Band Director",
+            selector: (row: { band_director_name: any }) =>
+                row.band_director_name,
+            omit: true,
+        },
+        {
+            name: "Band Director Phone",
+            selector: (row: { band_director_phone: any }) =>
+                row.band_director_phone,
+            omit: true,
+        },
+        {
+            name: "Band Director Email",
+            selector: (row: { band_director_email: any }) =>
+                row.band_director_email,
+            omit: true,
+        },
+        {
+            name: "Booster Parent Name",
+            selector: (row: { booster_parent_name: any }) =>
+                row.booster_parent_name,
+            omit: true,
+        },
+        {
+            name: "Booster Parent Phone",
+            selector: (row: { booster_parent_phone: any }) =>
+                row.booster_parent_phone,
+            omit: true,
+        },
+        {
+            name: "Booster Parent Email",
+            selector: (row: { booster_parent_email: any }) =>
+                row.booster_parent_email,
+            omit: true,
+        },
+        {
+            name: "Parade March Title",
+            selector: (row: { parade_march_title: any }) =>
+                row.parade_march_title,
+            omit: true,
+        },
+        {
+            name: "Parade March Composer",
+            selector: (row: { parade_march_composer: any }) =>
+                row.parade_march_composer,
+            omit: true,
+        },
+        {
+            name: "Additional Band Staff Names",
+            selector: (row: { additional_band_staff_names: any }) =>
+                row.additional_band_staff_names,
+            omit: true,
+        },
+        {
+            name: "Drum Major",
+            selector: (row: { drum_major: any }) => row.drum_major,
+            omit: true,
+        },
+        {
+            name: "Drum Major Name",
+            selector: (row: { drum_major_name: any }) => row.drum_major_name,
+            omit: true,
+        },
+        {
+            name: "Color Guard Advisor",
+            selector: (row: { color_guard_advisor: any }) =>
+                row.color_guard_advisor,
+            omit: true,
+        },
+        {
+            name: "Color Guard Captains",
+            selector: (row: { color_guard_captains: any }) =>
+                row.color_guard_captains,
+            omit: true,
+        },
+        {
+            name: "Drill Team",
+            selector: (row: { drill_team: any }) => row.drill_team,
+            omit: true,
+        },
+        {
+            name: "Drill Team Advisor",
+            selector: (row: { drill_team_advisor: any }) =>
+                row.drill_team_advisor,
+            omit: true,
+        },
+        {
+            name: "Color Guard Captains",
+            selector: (row: { color_guard_captains: any }) =>
+                row.color_guard_captains,
+            omit: true,
+        },
+        {
+            name: "School Enrollment",
+            selector: (row: { school_enrollment: any }) =>
+                row.school_enrollment,
+            omit: true,
+        },
+        {
+            name: "Number of Students in Band",
+            selector: (row: { number_of_students_in_band: any }) =>
+                row.number_of_students_in_band,
+            omit: true,
+        },
+        {
+            name: "Number of Students in Color Guard",
+            selector: (row: { number_of_students_in_color_guard: any }) =>
+                row.number_of_students_in_color_guard,
+            omit: true,
+        },
+        {
+            name: "Number of Students in Drill Team",
+            selector: (row: { number_of_students_in_drill_team: any }) =>
+                row.number_of_students_in_drill_team,
+            omit: true,
+        },
+        {
+            name: "Number of Buses",
+            selector: (row: { number_of_buses: any }) => row.number_of_buses,
+            omit: true,
+        },
+        {
+            name: "Number of Box Trucks",
+            selector: (row: { number_of_box_trucks: any }) =>
+                row.number_of_box_trucks,
+            omit: true,
+        },
+        {
+            name: "Number of Trailers",
+            selector: (row: { number_of_trailers: any }) =>
+                row.number_of_trailers,
+            omit: true,
+        },
+        {
+            name: "Number of Tractor Trailer Rigs",
+            selector: (row: { number_of_tractor_trailer_rigs: any }) =>
+                row.number_of_tractor_trailer_rigs,
+            omit: true,
+        },
+        {
+            name: "Special Instructions",
+            selector: (row: { special_instructions: any }) =>
+                row.special_instructions,
+            omit: true,
         },
         {
             cell: (row: TParticipant) => (
@@ -325,12 +589,15 @@ const Dashboard = () => {
                     >
                         Invite Participant
                     </Button>
-                    <DataTable
+
+                    <ExportCSV />
+
+                    {/* <DataTable
                         columns={columns}
                         data={participants}
                         selectableRows
                         pagination
-                    />
+                    /> */}
                 </Box>
             </SidebarWithHeader>
         </ChakraProvider>
--- client/src/features/types/participant.ts
+++ client/src/features/types/participant.ts
@@ -13,24 +13,24 @@ export interface TParticipant {
     booster_parent_name?: string;
     booster_parent_phone?: string;
     booster_parent_email?: string;
-    parade_march_title ?: string;
+    parade_march_title?: string;
     parade_march_composer?: string;
     additional_band_staff_names?: string;
     drum_major?: string;
     drum_major_name?: string;
-    color_guard_advisor?: string;
+    color_guard_advisor?: string;//
     color_guard_captains?: string;
     drill_team?: string;
     drill_team_advisor?: string;
-    drill_team_captains?: string;
+    drill_team_captains?: string;//
     school_enrollment?: string;
-    number_of_students_in_band?: number;
-    number_of_students_in_color_guard?: number;
-    number_of_students_in_drill_team?: number;
-    number_of_buses?: number;
-    number_of_box_trucks?: number;
-    number_of_trailers?: number;
-    number_of_tractor_trailer_rigs?: number;
+    number_of_students_in_band?: string;
+    number_of_students_in_color_guard?: string;
+    number_of_students_in_drill_team?: string;
+    number_of_buses?: string;
+    number_of_box_trucks?: string;
+    number_of_trailers?: string;
+    number_of_tractor_trailer_rigs?: string;
     special_instructions?: string;
     event?: string | null
 }
