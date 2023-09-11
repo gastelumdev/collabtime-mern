c139809c42cba7a90a951bc0c17d6743c4decc52
Omar Gastelum <gastelumdev@gmail.com>
2023-09-11 15:15:53 -0700

    Update Cors

--- client/src/features/api/api_url.ts
+++ client/src/features/api/api_url.ts
@@ -4,7 +4,7 @@ const DEBUG = process.env.REACT_APP_DEBUG || "false";
 
 const API_URL = (process.env.REACT_APP_DEBUG == "true")
     ? "http://localhost:4000"
-    : "https://collabtime-mern-backend.onrender.com";
+    : "https://collabtime-be.onrender.com";
 
 console.log(API_URL)
 
